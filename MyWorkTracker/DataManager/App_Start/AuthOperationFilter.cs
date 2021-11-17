1f2aa28ced920b706b85b800c72e45f792f408b9
Isaac Byron <i.byron@hotmail.com>
2021-11-16 18:32:05 -0800

    Add Swagger
    Added swagger and configured it

---
+++ MyWorkTracker/DataManager/App_Start/AuthOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace DataManager.App_Start
+{
+    // this class adds an authorization parameter to every api method so that we can plug in our access token if we have one and
+    // get access to certain endpoints
+
+    public class AuthOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in  = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
---
+++ MyWorkTracker/DataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,54 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace DataManager.App_Start
+{
+
+    // this class allows us to get an access token using the username and password that we register for an account with
+
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-endcoded"
+                    },
+                    parameters = new List<Parameter>{
+                            new Parameter
+                            {
+                                type = "string",
+                                name  ="grant_type",
+                                required = true,
+                                @in = "formData",
+                                @default = "password"
+                            },
+                            new Parameter
+                            {
+                                type = "string",
+                                name = "username",
+                                required = true,
+                                @in = "formData"
+                            },
+                            new Parameter
+                            {
+                                type = "string",
+                                name  = "password",
+                                required =  true,
+                                @in  = "formData"
+                            }
+                        }
+                }
+            });
+        }
+    }
+}
